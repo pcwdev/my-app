c6393bc6aacc64c898db01c76a47607827365d7b
pcwdev <sos04711@naver.com>
2026-04-13 21:27:01 +0900

    first commitdsadasdasdasdasdasdgit push -u origin main t push -u origin main

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>🔥 내 커뮤니티 시작</h1>
+      <p>여기서 트래픽 모은다</p>
+
+      <button style={{ padding: 10, marginTop: 20 }}>글 쓰러 가기</button>
+    </main>
+  )
+}
